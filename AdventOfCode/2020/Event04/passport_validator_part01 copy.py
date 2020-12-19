42b7738acd7519fcc7acfa9336a09edcd500b7e6
usamasadiq <usama.sadiq@arbisoft.com>
2020-12-20 04:00:37 +0500

    AdventOfCode Event04 Passport Validator

---
+++ AdventOfCode/2020/Event04/passport_validator_part01 copy.py
@@ -0,0 +1,38 @@
+"""
+This program reads data of some passports from a file
+and validates each of them based on input criteria.
+"""
+
+data_file = 'passports.txt'
+required_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid']
+allowed_missing_fields = ['cid']
+
+def parse_passports(passport_file):
+    passports = []
+    with open(passport_file, 'r') as f:
+        passport = {}
+        while line := f.readline():
+            fields = line.split()
+            if fields:
+                for field in fields:
+                    key, value = field.split(':')
+                    passport[key] = value
+            else:  # empty line
+                passports.append(passport)
+                passport = {}
+        passports.append(passport)  # handle the last entry from data file
+    return passports
+
+def validate_passports(passports, req_fields, allowed_fields):
+    valid_count = 0
+    for passport in passports:
+        if set(req_fields).issubset(passport.keys()):  # case A: where all fields are present
+            valid_count += 1
+        else:  # case B: where some keys are missing
+            missing_filds = set(req_fields) - passport.keys()
+            if missing_filds.issubset(allowed_fields):  # case C: where only field 'cid' is missing
+                valid_count += 1
+    return valid_count
+
+input_passports = parse_passports(data_file)
+print(f'Valid Passwords: {validate_passports(input_passports, required_fields, allowed_missing_fields)}')
---
+++ AdventOfCode/2020/Event04/validations.py
@@ -0,0 +1,61 @@
+import re
+
+def validate_byr(value):
+    if re.match('^[0-9]{4}', value):
+        if 1920 <= int(value) <= 2002:
+            return True
+    return False
+
+def validate_iyr(value):
+    if re.match('^[0-9]{4}', value):
+        if 2010 <= int(value) <= 2020:
+            return True
+    return False
+
+def validate_eyr(value):
+    if re.match('^[0-9]{4}', value):
+        if 2020 <= int(value) <= 2030:
+            return True
+    return False
+
+def validate_hgt(value):
+    if re.match('^[0-9]+(cm|in)$', value):
+        if 'cm' in value:
+            if 150 <= int(value.strip('cm')) <= 193:
+                return True
+        elif 'in' in value:
+            if 59 <= int(value.strip('in')) <= 76:
+                return True
+    return False
+
+def validate_hcl(value):
+    if re.match('^#([0-9]|[a-f]){6}', value):
+        return True
+    return False
+
+def validate_ecl(value):
+    if re.match('amb|blu|brn|gry|grn|hzl|oth', value):
+        return True
+    return False
+
+def validate_pid(value):
+    if re.match('^[0-9]{9}$', value):
+        return True
+    return False
+
+def validate_fields(passport):
+    if not validate_byr(passport['byr']):
+        return False
+    if not validate_eyr(passport['eyr']):
+        return False
+    if not validate_iyr(passport['iyr']):
+        return False
+    if not validate_ecl(passport['ecl']):
+        return False
+    if not validate_hcl(passport['hcl']):
+        return False
+    if not validate_hgt(passport['hgt']):
+        return False
+    if not validate_pid(passport['pid']):
+        return False
+    return True
